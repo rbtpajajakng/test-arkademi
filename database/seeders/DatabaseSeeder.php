b38c8af1c58a77fd2b42d4459f0bd7fbf17fbb24
Robertus Pajajakng <robert.pajajakng@gmail.com>
2022-09-01 12:02:06 +0700

    Add product category model, add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\CategoryProduct;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        // Seed product category table
+        for ($i=1; $i <= 5; $i++) {
+            $categoryProduct = new CategoryProduct;
+            $categoryProduct->category_name = "category_{$i}";
+            $categoryProduct->save();
+        }
+
+        // Seed product table
+        for ($i=1; $i <= 10; $i++) { 
+            $product = new Product;
+            $product->product_name = "product_{$i}";
+            $product->price = rand(10000, 200000);
+            $product->stock = rand(0, 1000);
+            $product->category_id = rand(1, 5);
+            $product->save();
+        }
     }
 }
